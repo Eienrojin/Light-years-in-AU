ca4e3681ba6f677610b6c875cd474c4086bdc9e8
Nikita Polunin (二木立・半人) <Stkchanelx@gmail.com>
2021-10-16 19:38:43 +0300

    Добавьте файлы проекта.

---
+++ Light years in AU/Light years in AU.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+
+void au();
+
+int main()
+{
+    au();
+}
+
+void au()
+{
+    double light_years, au;
+    cout << "Enter the number of light years: ";
+    cin >> light_years;
+
+    au = light_years * 63240;
+    cout << light_years << " light years = " << au << " astronomical units." << endl;
+}
